37d9a9109cfbb977e1318660fca0519cacb6eec7
g.olt <olt.egor@gmail.com>
2022-12-12 01:41:52 +0300

    refctoring

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,6 +1,10 @@
+import { BigNumber } from "ethers"
+
 // mumbai addresses
 export const daiAddress = '0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F'
 export const collateralPollAddress = '0xD6dE6Ca4dcc9Bf3e8BbC4130725A7D795B40c812'
 export const lowCollateralPollAddress = '0xd0b15fB519A21E634D7B82e470d70A99E819B7A6'
 export const erc1155Address = 'enter your ERC1155 address'
 export const erc721Address = 'enter your ERC721 address'
+export const erc1155TokenId = BigNumber.from('enter your ERC1155 token id')
+export const erc721TokenId = BigNumber.from('enter your ERC721 token id')
